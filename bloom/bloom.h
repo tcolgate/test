10ea91f440d2164de662438e3f42a6be0f28ecf6
Tristan Colgate-McFarlane <tcolgate@gmail.com>
2015-10-17 16:05:10 +0100

    Tidy up some of hte types, expand the bloom test

--- bloom/bloom.h
+++ bloom/bloom.h
@@ -1,25 +1,26 @@
 #include <stdlib.h>
 #include <stdbool.h>
+#include <inttypes.h>
 #include <math.h>
 #include "lookup3.h"
 
 #include <stdio.h>
 
-typedef char* bitset;
-bitset bitset_new(int size){
+typedef uint8_t* bitset;
+bitset bitset_new(uint32_t size){
   return (bitset) calloc(1 + size/8,sizeof(char));
 };
 
-void bitset_set(bitset bs, unsigned int n){
-  int B = n / 8;
-  int b = n % 8;
-  bs[B] = bs[B] | (1 << b);
+void bitset_set(bitset bs, uint32_t n){
+  uint32_t B = n / 8;
+  uint32_t b = n % 8;
+  bs[B] = bs[B] | ((uint8_t)1 << b);
 };
 
-bool bitset_isset(bitset bs, unsigned int n){
-  int B = n / 8;
-  int b = n % 8;
-  return ((bs[B] & (1 << b)) == (1<<b));
+bool bitset_isset(bitset bs, uint32_t n){
+  uint32_t B = n / 8;
+  uint32_t b = n % 8;
+  return ((bs[B] & (1 << b)) == ((uint8_t)1<<b));
 };
 
 void bitset_clear(bitset b){
@@ -28,25 +29,25 @@ void bitset_clear(bitset b){
 typedef struct bloom {
   uint64_t count;
   bitset bits;
-  unsigned int m;
-  unsigned int k;
+  uint32_t m;
+  uint8_t k;
 }* bloom_t;
 
-bloom_t bloom_new(int n, double fprate){
-  float fillRatio = 0.5;
+bloom_t bloom_new(uint64_t n, double fprate){
+  double fillRatio = 0.5;
 
   bloom_t b = (bloom_t) malloc(sizeof(bloom_t));
 
-  b->m = (unsigned int) ceil((double)n / (log10(fillRatio) *
+  b->m = (uint32_t) ceil((double)n / (log10(fillRatio) *
                   log10(1-fillRatio)) / fabs(log(fprate)));
-  b->k = (unsigned int) ceil(log2(1/fprate));
+  b->k = (uint8_t) ceil(log2(1/fprate));
 
   b->bits = bitset_new(b->m);
 
   return b;
 }
 
-bool bloom_test(bloom_t b, void* key, int len){
+bool bloom_test(bloom_t b, void* key, uint32_t len){
   uint32_t h=0,l=0;
   hashlittle2(key,len,&h,&l);
   
@@ -59,7 +60,7 @@ bool bloom_test(bloom_t b, void* key, int len){
   return true;
 };
 
-void bloom_add(bloom_t b, void* key, int len){
+void bloom_add(bloom_t b, void* key, uint32_t len){
   uint32_t h=0,l=0;
   hashlittle2(key,len,&h,&l);
 
--- bloom/tests/bloom_test.c
+++ bloom/tests/bloom_test.c
@@ -6,11 +6,25 @@ int main(int argc, char** argv){
   char *othing = "thing2";
 
   bloom_t b = bloom_new(1000,0.001);
-  printf ("m: %d\n",b->m);
-  printf ("k: %d\n",b->k);
 
-  bloom_add(b,othing,strlen(othing)+1);
+  if (b->m != 1598)
+    return 1;
+  if (b->k != 10)
+    return 1;
 
-  return bloom_test(b,thing,strlen(thing)+1) ? 0 : 1;
+  if (bloom_test(b,thing,strlen(thing)+1))
+    return 1;
+
+  bloom_add(b,thing,strlen(thing)+1);
+
+  if (b->count != 1)
+    return 1;
+
+  if (!bloom_test(b,thing,strlen(thing)+1))
+    return 1;
+  if (bloom_test(b,othing,strlen(othing)+1))
+    return 1;
+
+  return 0;
 }
 
